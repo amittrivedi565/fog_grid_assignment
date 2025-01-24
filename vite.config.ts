e1de4494fd51fb50a74695ab4cd327e95c2de2fb
Amit Trivedi <amittrivedi@Amits-MacBook-Air.local>
2025-01-25 00:23:30 +0530

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/FOG_ASSIGNMENT_GRID_WAVE",
+    base: "/fog_grid_wave_assignment",
     plugins: [react()],
 });
